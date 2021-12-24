9f8c94c724aee703770ab7c31134f460eaa36434
Liz Thomas <thomas.elizabeth.k@gmail.com>
2021-12-24 11:35:43 -0800

    Fix typo

--- HairSalon/Models/HairSalonContext.cs
+++ HairSalon/Models/HairSalonContext.cs
@@ -5,7 +5,7 @@ namespace HairSalon.Models
   public class HairSalonContext : DbContext
   {
     public DbSet<Client> Clients { get; set; }
-    public DbSet<Stylist> Sytlists { get; set; }
+    public DbSet<Stylist> Stylists { get; set; }
 
     public HairSalonContext(DbContextOptions options) : base(options) { }
 
