81779481dca771679b81efe75b2950de9146115f
Liz Thomas <thomas.elizabeth.k@gmail.com>
2021-12-24 10:51:50 -0800

    Fix typo in using directive

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetcore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
