c1d80e887b05f5e4d7d7bc703d94a87383e1d455
Liz Thomas <thomas.elizabeth.k@gmail.com>
2021-12-24 11:15:40 -0800

    Fix typo

--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
 using System.Collections.Generic;
-using BestRestaurants.Models;
+using HairSalon.Models;
 
 namespace HairSalon.Controllers
 {
