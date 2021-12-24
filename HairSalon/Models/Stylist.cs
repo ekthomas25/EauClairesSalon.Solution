972d1a80fee55fd927ce5fad959253bca09c5790
Liz Thomas <thomas.elizabeth.k@gmail.com>
2021-12-24 11:23:09 -0800

    Add properties, HashSet

---
+++ HairSalon/Models/Stylist.cs
@@ -0,0 +1,16 @@
+namespace HairSalon.Models
+{
+  public class Stylist
+  {
+    public Stylist()
+    {
+      this.Clients = new HashSet<Client>();
+    }
+    
+    public int StylisttId { get; set; }
+    public string Name { get; set; }
+    public string Specialty { get; set; }
+
+    public virtual ICollection<Client> Clients { get; set; }
+  }
+}
